7d266925944016f5a585238e02273f299f5afd15
SamuelChan1433 <929316208@qq.com>
2017-07-16 13:51:47 +0800

    main page Html and css

---
+++ forumSystem/js/form.js
@@ -0,0 +1,8 @@
+$(function () {
+	
+	var html="";
+	html += '<img src="img/logo.jpg" class="logo">';
+	html += '<div id="#login" class="LoginSighupBTN">登陆</div>';
+	html += '<div id="#sighup" class="LoginSighupBTN">注册</div>';
+	$("#header").html(html);
+});
